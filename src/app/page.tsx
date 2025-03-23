928f25c3a55b50f62ddbe095e840d76aeafbef4f
xdjklr <reyeskyle@gmail.com>
2025-03-23 23:59:29 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+
   return (
     <main className=" ">
-      wassup
+      <div className="flex flex-wrap justify-items-center gap 4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+      </div>
     </main>
   );
 }
